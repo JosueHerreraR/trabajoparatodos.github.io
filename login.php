5fc9bd9a5d312a55a7372d0c69e1736352c571f2
Joel Josue Herrera Reyes <92177163+JosueHerreraR@users.noreply.github.com>
2022-10-19 22:18:26 -0500

    Cambio de maquetacion

--- login.php
+++ login.php
@@ -1,47 +1,39 @@
-<html lang="es">
+<!DOCTYPE html>
+<html lang="en">
     <head>
-        <meta charset="utf-8">
+        <meta charset="UTF-8">
+        <meta name="autor" content="Abdiel Cen, Luis Chuc, Joel Herrera, Jose Poot y Wilfrido Puc"/>
+        <meta name="copyright" content=""/> 
+        <meta name="description" content="Sistema de Control Escolar de Educacion Media Superior">
+        <link rel="icon" type="image/jpg" href="">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Inició de sección</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="css/normalize.css">
+        <title>Inicio de sección</title>
+        <link rel="shortcut icon" href="../img/icono_paems.jpg" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link rel="stylesheet" href="css/estilos.css">
         <link rel="stylesheet" href="css/login.css">
-        <!-- <link rel="stylesheet" href="css/aviso_login.css"> -->
     </head>
+
     <body>
-        <div class="contenedor-formulario contenedor">
-            <div class="imagen-formulario">   
+        
+        <div class="container-form">
+            <div class="form-register">
+                <form action="controlador/controlador_validar.php" class="formulario" method="post">
+                    <img src="img/QROO.webp" class="avatar" alt="Avatar Image">
+                    <h2>Bienvenidos</h2>
+                    <h3>Inicio de sesión</h3>
+                        <?php
+                            include "modelo/conexion.php";
+                        ?>
+                    <label class="label" for="usuario">Usuario</label>
+                    <input name="usuario" id="usuario" class="controls" type="text" placeholder="Ingresa tu Usuario">
+                    <label class="label" for="contraseña">Contraseña</label>
+                    <input name="contraseña" id="input" class="controls" type="password" placeholder="Ingresa tu Contraseña">
+                    <input class="submit-btn" type="submit" value="Iniciar Sesion">
+                </form>
             </div>
-            <form class="formulario" method="post">
-                <div class="texto-formulario">
-                    <h2>Bienvenido de nuevo</h2>
-                    <p>Inicia sesión con tu cuenta</p>
-                </div>
-                <?php
-                    include "modelo/conexion.php";
-                    include "controlador/controlador_login.php";
-                ?>
-                <div class="input">
-                    <label for="usuario">Usuario</label>
-                    <input placeholder="Ingresa tu nombre" type="text" id="usuario" name="usuario">
-                </div>
-                <div class="input">
-                    <label for="contraseña">Contraseña</label>
-                    <input placeholder="Ingresa tu contraseña" type="password" id="input" name="password">
-                </div>
-                <div class="password-olvidada">
-                    <a href="#">¿Olvidaste tu contraseña?</a>
-                </div>
-                <div class="password-olvidada">
-                    <a href="registro_usuario.php">Registrate</a>
-                </div>
-                <div class="input">
-                    <input name="btningresar" type="submit" value="Iniciar Sesión">
-                </div>
-            </form>
         </div>
+
     </body>
 </html>
